c5ea7030622964e090ee10d3728f1609b2e1fcba
Strong <justbefree@126.com>
2022-08-10 17:35:48 +0800

    修改文档信息

--- docs/.vuepress/components/about/us.vue
+++ docs/.vuepress/components/about/us.vue
@@ -3,7 +3,7 @@
     <ul>
       <li>
         <img src="./images/logo.png">
-        <p>Dullarjs developed by <a target="_blank" href="https://dullar.xyz/">Yuanian cloud front-end team</a></p>
+        <p>Dullarjs developed by <a target="_blank" href="https://dullar.xyz/">Dullar front-end team</a></p>
         <p>You can follow us on <a target="_blank" href="https://github.com/dullarjs">Github</a></p>
         <p>Get badge for <a target="_blank" href="https://badge.fury.io/for/js/@dullar/cli?type=svg">@dullar</a></p>
       </li>
